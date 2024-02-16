fc3870cf66de162898241b2f422c78ec4a3714d1
sagar <sagar201993>
2024-02-16 17:33:44 +0100

    sign up page ui completed

--- api/index.js
+++ api/index.js
@@ -22,6 +22,8 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+//handling errors
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "internal server error";

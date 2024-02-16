e3cfe9edc9af9f3daf19cf53936e867b109937d7
sagar <sagar201993>
2024-02-16 12:42:04 +0100

    databse_connect

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.listen(3000, () => {
-  console.log("server is running 3000!");
+  console.log("server is running 3000");
 });
